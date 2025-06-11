6913233b9298e888eabf5bf9b1523cc005d569cc
oyinade olawoyin <oyinadeolawoyin@proton.me>
2025-06-11 10:31:43 +0100

    Add react url to cors origin

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
   
 app.use(cors({
-    origin: ["http://localhost:5175"],
+    origin: ["http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
 }));

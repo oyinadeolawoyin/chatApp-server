29a90c5d4410ed9acddb08dbf23569081e4cd415
oyinade olawoyin <oyinadeolawoyin@proton.me>
2025-06-11 11:16:02 +0100

    Add frontend origin to my cors

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
   
 app.use(cors({
-    origin: ["http://localhost:5173"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
+  origin: ["http://localhost:5173"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
 }));
   
 app.use(cookieParser());
